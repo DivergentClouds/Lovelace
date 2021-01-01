67c0a019fca794829de5b8140bab2487a7916269
Rain <27089165+RaindropBloom@users.noreply.github.com>
2021-01-01 16:11:26 -0600

    fixed some indenting in interrupt-handler.asm

--- tools/assembly-progs/os/interrupt-handler.asm
+++ tools/assembly-progs/os/interrupt-handler.asm
@@ -16,14 +16,14 @@ LOD $E000 R1    ; 1 if peripheral is interrupting, 0 if not
 CMP R1 #0       ; check if peripheral is interrupting
 BRA Z :SKIP     ; if peripheral is not interrupting then branch to :SKIP
 
-JSR $FF00		; jump to peripheral's interrupt subroutine
+JSR $FF00       ; jump to peripheral's interrupt subroutine
 
-STR #0 $E000	; mark peripheral as not interrupting
+STR #0 $E000    ; mark peripheral as not interrupting
 
 :SKIP
-DEC R0			; go to the next peripheral
-CMP R0 #1		; check if the peripheral number is 1
-BRAN Z :LOOP	; if there are more peripherals branch back to :LOOP
+DEC R0          ; go to the next peripheral
+CMP R0 #1       ; check if the peripheral number is 1
+BRAN Z :LOOP    ; if there are more peripherals branch back to :LOOP
 
 POP ACC         ; pop all registers from the stack back into the correct place
 POP R3
