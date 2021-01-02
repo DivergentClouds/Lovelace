0f6528a37ad04d19cccc9e10c5dae260a4da9792
Rain <27089165+RaindropBloom@users.noreply.github.com>
2021-01-01 21:28:38 -0600

    updated some stuff to use new bank size

--- tools/assembly-progs/audio/space-press.asm
+++ tools/assembly-progs/audio/space-press.asm
@@ -1,34 +1,34 @@
-STR		#3 		$9FFF 	; switch to audio bank
-STR		#0 		$A002 	; switch to oscillator 0
-STR		#2 		$A003 	; switch to waveform register
-STR		#%00	$A004 	; set to pulse
-STR		#3 		$A003 	; switch to pulse width register
-STR		#$A0	$A004 	; set to 50% (128)
-STR		#1 		$A003 	; switch to lowpass register
-STR		#100	$A004 	; set to 100
-STR		#4 		$A003 	; switch to note register
-STR		#49		$A004	; set note to 49 (C4)
-STR		#3 		$9FFF 	; switch to audio bank
-STR		#7		$A003	; switch to volume register
-LOD		#$A0	ACC
+STR		#3 		$DFFF 	; switch to audio bank
+STR		#0 		$E002 	; switch to oscillator 0
+STR		#2 		$E003 	; switch to waveform register
+STR		#%00	$E004 	; set to pulse
+STR		#3 		$E003 	; switch to pulse width register
+STR		#$E0	$E004 	; set to 50% (128)
+STR		#1 		$E003 	; switch to lowpass register
+STR		#100	$E004 	; set to 100
+STR		#4 		$E003 	; switch to note register
+STR		#49		$E004	; set note to 49 (C4)
+STR		#3 		$DFFF 	; switch to audio bank
+STR		#7		$E003	; switch to volume register
+LOD		#$E0	ACC
 
 :LOOP
-STR		#1 		$9FFF 	; switch to keyboard bank
-LOD		$A00A	R0		; 1 if spacebar is pressed 0 if not
+STR		#1 		$DFFF 	; switch to keyboard bank
+LOD		$E00A	R0		; 1 if spacebar is pressed 0 if not
 CMP		R0		#1		; Check if R0 is 1
 BRA		Z		:ON		; if 1 jump to volume on
 BRAN	Z		:OFF	; if 0 jump to volume off
 
 :ON
-STR		#3 		$9FFF 	; switch to audio bank
-STR		#3 		$A003 	; switch to pulse width register
+STR		#3 		$DFFF 	; switch to audio bank
+STR		#3 		$E003 	; switch to pulse width register
 ADD		#1
-STR		ACC		$A004
-STR		#7		$A003	; switch to volume register
-STR		#$40	$A004	; set volume to 64
+STR		ACC		$E004
+STR		#7		$E003	; switch to volume register
+STR		#$40	$E004	; set volume to 64
 JMP		:LOOP
 
 :OFF
-STR		#3 		$9FFF 	; switch to audio bank
-STR		#0		$A004	; set volume to 0
+STR		#3 		$DFFF 	; switch to audio bank
+STR		#0		$E004	; set volume to 0
 JMP		:LOOP
